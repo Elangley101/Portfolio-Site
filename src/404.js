2f4cff588a00c37ab57032980d2ac617f2e95f82
Ethan Langley <98287743+Elangley101@users.noreply.github.com>
2025-07-29 14:41:41 -0400

    Create 404.js

---
+++ src/404.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for doesn't exist or has been moved.</p>
+      <a href="/">Go back to homepage</a>
+    </div>
+  );
+}
